58f22b7a2d041473274a2165cf9108372aa3b1d5
Nongoogle <essamsleem9@gmail.com>
2026-09-24 21:27:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET106_OOP_Assignment05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
