66f6ce4e7cb60afd3b7083c74591b260d10bf5e9
Nongoogle <essamsleem9@gmail.com>
2026-09-24 23:54:11 +0300

    Part02 Question07

--- Program.cs
+++ Program.cs
@@ -2,6 +2,20 @@
 {
     internal class Program
     {
+        public static class DeliveryUtilities
+        {
+            public static void PrintSeparator()
+            {
+                Console.WriteLine("==========================================");
+            }
+
+            public static void PrintSystemTitle()
+            {
+                PrintSeparator();
+                Console.WriteLine("North sinai Delivery System");
+                PrintSeparator();
+            }
+        }
 
         static void Main(string[] args)
         {
