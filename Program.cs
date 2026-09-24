540676ffa6f56cdd4abb26e2bd3f7612943d32fd
Nongoogle <essamsleem9@gmail.com>
2026-09-24 21:37:09 +0300

    Part01 Question01 C

--- Program.cs
+++ Program.cs
@@ -13,6 +13,14 @@
             //b) Does assigning one object to another create a new object? Explain.
 
             //no it does not the both virables reference to the same object
+
+            //c) What is the difference between copying an object and copying its reference?
+
+            /*
+             copy the reference mean two virable refer to the same object
+
+             copy the object is creating a new separate object
+            */
             #endregion
             #endregion
         }
