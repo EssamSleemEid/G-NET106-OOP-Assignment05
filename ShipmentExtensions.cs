88de9a0b90bef75e88b887566b05bcee1b355cfe
Nongoogle <essamsleem9@gmail.com>
2026-09-25 00:06:03 +0300

    Part02 Question08

---
+++ ShipmentExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET106_OOP_Assignment05
+{
+    public static class ShipmentExtensions
+    {
+        public static string GetSummary(this Shipment shipment)
+        {
+            string shipmentType=shipment.GetType().Name;
+
+            return "tracking code : " + shipment.TrackingCode + " - " + "shipment type : " + shipmentType + " - " + "the wight : " + shipment.Weight + " KG " + " - " + "status : " + shipment.GetTrackingStatus();
+        }
+        public static bool IsDelivered(this Shipment shipment)
+        {
+            return shipment.GetTrackingStatus() == "Delivered"; ;
+        }
+    }
+}
